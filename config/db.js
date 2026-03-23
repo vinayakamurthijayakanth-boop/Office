b5d91714964f32e3934180eda94b0dfbf0b84139
vinayakamurthijayakanth-boop <vinayakamurthijayakanth@gmail.com>
2026-03-23 22:00:35 +0530

    Add config/db.js

---
+++ config/db.js
@@ -0,0 +1,17 @@
+// This file is generally not needed with direct connection in server.js but kept for structure or complex setups
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB Connected...');
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// module.exports = connectDB;
